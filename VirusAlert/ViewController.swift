3e68cb8cb06d2c294a32a1f673902d8475ea8f87
Seow Yung Hoe <seowyunghoe@hotmail.com>
2017-03-01 12:56:47 +0800

    Initial Commit

---
+++ VirusAlert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirusAlert
+//
+//  Created by Seow Yung Hoe on 01/03/2017.
+//  Copyright © 2017 Seow Yung Hoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
